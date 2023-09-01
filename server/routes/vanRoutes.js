360de4e95cd4de2b3fc2b80e88e03ce327aa60f7
Olayinka <olayinka4grace@gmail.com>
2023-09-01 19:44:46 +0100

    commit

--- server/routes/vanRoutes.js
+++ server/routes/vanRoutes.js
@@ -13,7 +13,7 @@ const requireAuth = require('../middlewares/requireAuth')
 
 
 router.get("/",  getAllVans);
-router.get("/:id",requireAuth, getVanById);
+router.get("/:id", getVanById);
 router.post("/", requireAuth, addVan);
 router.put("/:id", requireAuth, updateVan);
 router.delete("/:id", requireAuth, deleteVan);
